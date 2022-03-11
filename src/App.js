94aa5af303d6ff5a7c6904f7593db15da4445b0e
Arhan Kamra <arhankamra@yahoo.com>
2022-03-12 01:26:45 +0530

    Sass starts working with react, just a start

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import "./index.scss";
-
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Styles from "./index.module.scss";
 
 // import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Footer />
       <Header />
+      <button className={Styles.btn}>button text</button>
       <button>button text</button>
-      <button>button text</button>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,13 @@
-import "./header.scss";
-
 import Navigation from "./comps/Navigation/Navigation";
 import React from "react";
+import Styles from "./header.module.scss";
 
 const Header = () => {
   return (
     <div>
       <Navigation />
-      <div>rasiya</div>
-      <div>rasiya</div>
+      <div className={Styles.btn}>rasiya</div>
+      <div className={Styles.btn2}>rasiya</div>
     </div>
   );
 };
